dfbeba411d4a9bd241cd0f00e5401a8e3510ea9a
cristian-spiescu <cristian.spiescu@crispico.com>
2018-09-11 14:51:29 +0300

    Added remote-object project.

---
+++ remote-object/src/main/java/com/crispico/flower_platform/remote_object/PleaseDeleteMeClaudiuServlet.java
@@ -0,0 +1,8 @@
+package com.crispico.flower_platform.remote_object;
+
+import javax.servlet.http.HttpServlet;
+
+@SuppressWarnings("serial")
+public class PleaseDeleteMeClaudiuServlet extends HttpServlet {
+
+}
