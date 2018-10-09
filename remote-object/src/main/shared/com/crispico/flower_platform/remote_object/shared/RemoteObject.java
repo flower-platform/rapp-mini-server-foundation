6e9fe39879649ff5bc6997f648ef05ea9f42d508
Claudiu Matei <claudiu.matei@shmessenger.org>
2018-10-09 15:49:37 +0300

    remote-object: added error callback

--- remote-object/src/main/shared/com/crispico/flower_platform/remote_object/shared/RemoteObject.java
+++ remote-object/src/main/shared/com/crispico/flower_platform/remote_object/shared/RemoteObject.java
@@ -100,7 +100,7 @@ public class RemoteObject {
 		return instanceName;
 	}
 	
-	public void invokeMethod(String method, Object[] arguments, ResultCallback callback) {
+	public void invokeMethod(String method, Object[] arguments, ResultCallback successCallback, ResultCallback errorCallback) {
 		if (requestSender == null) {
 			throw new IllegalStateException("The RemoteObject is not initialized; i.e. requestSender is null. It should be initialized by an IRemoteObjectInitializer.");
 		}
@@ -130,21 +130,23 @@ public class RemoteObject {
 		} else {
 			url = "hub";
 		}
-		requestSender.sendRequest("http://" + remoteAddress + "/" + url, sb.toString(), new RemoteObjectResponseCallback(callback));
+		requestSender.sendRequest("http://" + remoteAddress + "/" + url, sb.toString(), new RemoteObjectResponseCallback(successCallback, errorCallback));
 	}
 
 	final class RemoteObjectResponseCallback implements ResponseCallback {
 
-		public ResultCallback clientCallback;
+		public ResultCallback successCallback;
+
+		public ResultCallback errorCallback;
 		
-		public RemoteObjectResponseCallback(ResultCallback clientCallback) {
-			this.clientCallback = clientCallback;
+		public RemoteObjectResponseCallback(ResultCallback successCallback, ResultCallback errorCallback) {
+			this.successCallback = successCallback;
 		}
 
 		@Override
 		public void onSuccess(Object result) {
 			System.out.println("-> " + result.toString());
-			if (clientCallback == null) {
+			if (successCallback == null) {
 				return;
 			}
 			FlowerPlatformRemotingProtocolPacket packet = new FlowerPlatformRemotingProtocolPacket(result.toString());
@@ -152,17 +154,21 @@ public class RemoteObject {
 			case 'R':
 				packet.nextField(); // callbackId
 				String value = packet.nextField(); // result value
-				clientCallback.run(value);
+				if (successCallback != null) {
+					successCallback.run(value);
+				}
 				break;
 			case 'P':
 				String callbackId = packet.nextField(); // callbackId
-				callbacks.put(callbackId, clientCallback);
+				callbacks.put(callbackId, successCallback);
 				break;
 			}
 		}
 	
 		public void onError(String message) {
-			System.out.println("asdasdasd");
+			if (errorCallback != null) {
+				errorCallback.run(message);
+			}
 		}
 		
 	}
